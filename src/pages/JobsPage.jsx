f9a5c89a6a83bd6acbcc9ddfcf1aaf195b119846
Apurva Naik <apurva.naik@edg.com.au>
2024-06-17 09:55:03 +0530

    Uploading project

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,10 @@
+import JobListings from "../components/JobListings";
+const JobsPage = () => {
+  return (
+    <section>
+      <JobListings isHome={false}/>
+    </section>
+  )
+}
+
+export default JobsPage
